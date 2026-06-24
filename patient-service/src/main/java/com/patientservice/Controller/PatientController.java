7fd2a99cc674fdc984caf480d4698a1ef19535da
Abhijit Sahoo <shawabhijit370@gmail.com>
2026-06-24 15:47:46 +0530

    patient-service features completed with the docker image and also patient-service-db image created

--- patient-service/src/main/java/com/patientservice/Controller/PatientController.java
+++ patient-service/src/main/java/com/patientservice/Controller/PatientController.java
@@ -4,6 +4,11 @@ import com.patientservice.DTO.PatientRequestDto;
 import com.patientservice.DTO.PatientResponseDto;
 import com.patientservice.DTO.validators.CreatePatientValidationGroup;
 import com.patientservice.service.PatientService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.groups.Default;
 import lombok.Builder;
@@ -18,16 +23,27 @@ import java.util.UUID;
 @RestController
 @RequestMapping("/patients")
 @RequiredArgsConstructor
+@Tag(name = "Patient" , description = "API for managing Patients.")
 public class PatientController {
 
     private final PatientService patientService;
 
     @GetMapping
+    @Operation(summary = "Get Patients")
     public ResponseEntity<List<PatientResponseDto>> getAllPatients() {
         return ResponseEntity.ok().body(patientService.getPatients());
     }
 
     @PostMapping
+    @Operation(
+            summary = "Create Patient",
+            description = "Creates a new patient record in the system."
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Patient created successfully"),
+            @ApiResponse(responseCode = "400", description = "Validation failed"),
+            @ApiResponse(responseCode = "500", description = "Internal server error")
+    })
     public ResponseEntity<PatientResponseDto> createPatient(
             @Validated({Default.class, CreatePatientValidationGroup.class}) @RequestBody PatientRequestDto patientRequestDto
     ) {
@@ -35,7 +51,18 @@ public class PatientController {
     }
 
     @PutMapping("/{id}")
+    @Operation(
+            summary = "Update Patient",
+            description = "Updates an existing patient's information using the provided patient ID. "
+                    + "Only valid patient records can be updated. "
+                    + "The registration date is not required during updates."
+    )
     public ResponseEntity<PatientResponseDto> updatePatient(
+            @Parameter(
+                    description = "Unique identifier of the patient",
+                    example = "7b8c9d12-45ab-4ef1-9876-123456789abc",
+                    required = true
+            )
             @PathVariable UUID id,
             @Validated({Default.class}) @RequestBody PatientRequestDto patientRequestDto
     ) {
@@ -43,6 +70,7 @@ public class PatientController {
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Delete a patient")
     public ResponseEntity<String> deletePatient(@PathVariable UUID id) {
         patientService.deletePatient(id);
         return ResponseEntity.ok().body("Patient is Deleted.");
--- patient-service/src/main/java/com/patientservice/DTO/PatientRequestDto.java
+++ patient-service/src/main/java/com/patientservice/DTO/PatientRequestDto.java
@@ -1,27 +1,50 @@
 package com.patientservice.DTO;
 
 import com.patientservice.DTO.validators.CreatePatientValidationGroup;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
+@Schema(description = "Patient creation/update request")
 public class PatientRequestDto {
+
     @NotBlank(message = "Name is required.")
     @Size(max = 100, message = "Name cannot exceed 100 characters.")
+    @Schema(
+            description = "Full name of the patient",
+            example = "John Doe"
+    )
     private String name;
 
     @NotBlank(message = "Email is required.")
     @Email(message = "Email should be valid.")
+    @Schema(
+            description = "Patient email address",
+            example = "john.doe@example.com"
+    )
     private String email;
 
     @NotBlank(message = "Address is required.")
+    @Schema(
+            description = "Residential address",
+            example = "123 Main Street, New York"
+    )
     private String address;
 
     @NotBlank(message = "Date of birth is required.")
+    @Schema(
+            description = "Date of birth",
+            example = "1995-05-20"
+    )
     private String dateOfBirth;
 
     @NotBlank(groups = CreatePatientValidationGroup.class , message = "Registered date is required.")
+    @Schema(
+            description = "Patient registration date",
+            example = "2025-06-20"
+    )
     private String registeredDate;
 }
