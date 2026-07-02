a1a0b06600c11a20001c92e5462edbd3404c6d2b
Abhijit Sahoo <shawabhijit370@gmail.com>
2026-07-02 20:06:44 +0530

    filter chain added in the api-gateway to validate the auth before accessing any patient data

--- api-gateway/src/main/java/com/pm/apigateway/filter/JwtValidationGatewayFilterFactory.java
+++ api-gateway/src/main/java/com/pm/apigateway/filter/JwtValidationGatewayFilterFactory.java
@@ -1,14 +1,46 @@
 package com.pm.apigateway.filter;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+
 
 @Component
 public class JwtValidationGatewayFilterFactory extends AbstractGatewayFilterFactory<Object> {
 
+    private final WebClient webClient;
+
+    public JwtValidationGatewayFilterFactory(
+            WebClient.Builder webClientBuilder ,
+            @Value("${auth.service.url}") String authServiceUrl) {
+        this.webClient = webClientBuilder.baseUrl(authServiceUrl).build();
+    }
+
     @Override
     public GatewayFilter apply(Object config) {
-        return null;
+        return (exchange , chain) -> {
+            String token = exchange.getRequest().getHeaders().getFirst("Authorization");
+            if (token == null || !token.startsWith("Bearer ")) {
+                exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+                return exchange.getResponse().setComplete();
+            }
+
+            return webClient.get()
+                    .uri("/api/validate")
+                    .header("Authorization", token)
+                    .retrieve()
+                    .bodyToMono(String.class)
+                    .flatMap( response -> {
+                        if ("Auth Validation Success.".equals(response)) {
+                            return chain.filter(exchange);
+                        }
+
+                        exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+                        return exchange.getResponse().setComplete();
+                    });
+        };
     }
 }
